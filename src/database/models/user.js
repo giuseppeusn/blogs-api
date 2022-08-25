97d5439d7f79a5ad4e07c4c4b0fc574de909afa8
Giuseppe <giuseppeuhlmann@gmail.com>
2022-08-25 19:23:48 -0300

    feat: create User model

---
+++ src/database/models/user.js
@@ -0,0 +1,21 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+      { foreignKey: 'userId', as: 'blogposts' });
+  }
+
+  return User;
+};
+
+module.exports = User;
