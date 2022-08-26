8a89be746564b7aa96bcb854a566f79afeb2d3bc
Giuseppe <giuseppeuhlmann@gmail.com>
2022-08-26 18:59:26 -0300

    feat: create /categories route

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const usersController = require('./database/controllers/usersController');
-const usersRoute = require('./database/routes/usersRoute');
+const { usersRoute, categoriesRoute } = require('./database/routes');
 
 const app = express();
 
@@ -8,5 +8,6 @@ app.use(express.json());
 
 app.post('/login', usersController.userLogin);
 app.use('/user', usersRoute);
+app.use('/categories', categoriesRoute);
 
 module.exports = app;
